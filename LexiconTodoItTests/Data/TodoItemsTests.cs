fb0d034cab37588e3cdda2348b606f01165bde37
michael <carl.michael.sjogren@gmail.com>
2021-10-11 15:45:22 +0200

    started small with testing

--- LexiconTodoItTests/Data/TodoItemsTests.cs
+++ LexiconTodoItTests/Data/TodoItemsTests.cs
@@ -1,7 +1,20 @@
+using LexiconTodoIt.Data;
+using TodoIt.Model;
+using Xunit;
+
 namespace LexiconTodoItTests
 {
     public class TodoItemsTests
     {
-        
+        private TodoItems todoItems;
+        [Fact]
+        public void AddNewTodo()
+        {
+            todoItems = new TodoItems();
+            var todo = todoItems.AddTodo("Something");
+            Assert.Single(todoItems.FindAll());
+            Assert.Equal(todo , todoItems.FindAll()[0]);
+        }
+
     }
 }
--- LexiconTodoItTests/Model/TodoTests.cs
+++ LexiconTodoItTests/Model/TodoTests.cs
@@ -1,7 +1,22 @@
+using System;
+using TodoIt.Model;
+using Xunit;
+
 namespace LexiconTodoItTests
 {
     public class TodoTests
     {
-        
+        private Todo todo;
+
+        [Fact]
+        public void TestCreate()
+        {
+            todo = new Todo(-1, "Something");
+
+            Assert.Throws<ArgumentNullException>(() => new Todo(-1, ""));
+            Assert.NotNull(todo);
+            Assert.False(todo.Done);
+            Assert.Null(todo.Assignee);
+        }
     }
 }
