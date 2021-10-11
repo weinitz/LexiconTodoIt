a8c35c4443535befa918b280d02315ac07d91729
Tim Weinitz <weinitz.tim@gmail.com>
2021-10-11 12:45:40 +0200

    Add model Todo

---
+++ LexiconTodoIt/Model/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoIt.Model;
+
+namespace LexiconTodoIt.Model
+{
+    class Todo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public Todo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            this.description = description;
+        } 
+
+    }
+}
