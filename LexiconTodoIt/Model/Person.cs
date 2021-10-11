eee89763f81865d399e5d988ca1239c2c8546488
Tim Weinitz <weinitz.tim@gmail.com>
2021-10-11 11:39:25 +0200

    Create model person and testcode

---
+++ LexiconTodoIt/Model/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TodoIt.Model
+{
+    public class Person
+    {
+        private readonly int personId;
+
+        private string firstName;
+
+        private string lastName;
+
+        public Person(int personId, string firstName, string lastName)
+        {
+            this.personId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public int PersonId => personId;
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException();
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException();
+
+                lastName = value;
+            }
+        }
+    }
+}
---
+++ LexiconTodoItTests/PersonTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TodoIt.Model;
+using Xunit;
+
+namespace TodoItTests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void ConstructorTests()
+        {
+            const int personId = 1;
+            const string firstName = "Tim";
+            const string lastName = "Weinitz";
+            var person = new Person(personId, firstName, lastName);
+            Assert.Equal(personId, person.PersonId);
+            Assert.Equal(firstName, person.FirstName);
+            Assert.Equal(lastName, person.LastName);
+        }
+
+        [Fact]
+        public void ConstructorEmptyName()
+        {
+            const int personId = 1;
+            const string firstName = "";
+            const string lastName = " ";
+
+            Assert.Throws<ArgumentNullException>(
+                () => new Person(personId, firstName, lastName)
+            );
+        }
+    }
+}
