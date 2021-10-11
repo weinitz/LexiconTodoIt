d40b3d6f890c4a3c35af9e78ee695c888bbb5515
Tim Weinitz <weinitz.tim@gmail.com>
2021-10-11 14:13:23 +0200

    Add People class

---
+++ LexiconTodoIt/Data/People.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TodoIt.Model;
+
+namespace LexiconTodoIt.Data
+{
+    class People
+    {
+        private static Person[] persons = new Person[0];
+
+        public int Size()
+        {
+            return persons.Length;
+        }
+
+        public Person[] FindAll()
+        {
+            return persons;
+        }
+
+        public Person FindById(int personId)
+        {
+            if (personId > Size())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return persons[personId];
+        }
+
+        public Person AddPerson(string firstName, string lastName)
+        {
+            var personId = PersonSequencer.NextPersonId();
+            var person = new Person(personId, firstName, lastName);
+
+            Array.Resize(ref People.persons, People.persons.Length + 1);
+
+            People.persons[People.persons.Length] = person;
+
+            return person;
+        }
+
+        public void Clear()
+        {
+            People.persons = Array.Empty<Person>();
+        }
+    }
+}
