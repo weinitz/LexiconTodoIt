7bbeee37371b8eb50982d5a09d3ded52f37c01c6
michael <carl.michael.sjogren@gmail.com>
2021-10-11 13:57:42 +0200

    static method nextId and reset , private field todoId

--- LexiconTodoIt/Data/TodoSequencer.cs
+++ LexiconTodoIt/Data/TodoSequencer.cs
@@ -2,6 +2,16 @@ namespace LexiconTodoIt.Data
 {
     public class TodoSequencer
     {
-        
+        private static int todoId;
+
+        public static int nextTodoId()
+        {
+            return todoId++;
+        }
+
+        public static void reset()
+        {
+            todoId = 0;
+        }
     }
 }
