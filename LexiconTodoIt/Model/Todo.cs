e11149055b3702171ee9fcc35d90e95559420d36
michael <carl.michael.sjogren@gmail.com>
2021-10-11 14:31:40 +0200

    Added error checking for description

--- LexiconTodoIt/Model/Todo.cs
+++ LexiconTodoIt/Model/Todo.cs
@@ -8,11 +8,24 @@ namespace TodoIt.Model
         private String description;
         private bool done;
         private Person assignee;
+        public int TodoId => todoId;
         public Todo(int todoId, String description)
         {
             this.todoId = todoId;
             this.description = description;
         }
+        
+        
 
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException();
+
+                description = value;
+            }
+        }
     }
 }
