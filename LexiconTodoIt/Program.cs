1df311ee7e7d87531652f88be68bf7147a023b1e
Tim Weinitz <weinitz.tim@gmail.com>
2021-10-11 11:25:25 +0200

    Add project files.

---
+++ LexiconTodoIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LexiconTodoIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
