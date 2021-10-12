e1f97665bea97b6fb485316df67efe6d5639ea83
michael <carl.michael.sjogren@gmail.com>
2021-10-12 10:26:12 +0200

    created a new test for Todo class

--- LexiconTodoItTests/Model/TodoTests.cs
+++ LexiconTodoItTests/Model/TodoTests.cs
@@ -7,15 +7,20 @@ namespace LexiconTodoItTests
     public class TodoTests
     {
         private Todo todo;
-
+        
         [Fact]
         public void TestCreate()
         {
             todo = new Todo(-1, "Something");
-            Assert.Throws<ArgumentNullException>(() => new Todo(-1, ""));
             Assert.NotNull(todo);
             Assert.False(todo.Done);
             Assert.Null(todo.Assignee);
+        }
+
+        [Fact]
+        public void EmptyDescriptionThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Todo(-1, ""));
         }
     }
 }
