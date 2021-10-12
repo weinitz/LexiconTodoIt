5d1e3b0f8df352066db9b3023b7c4466a62ba9f3
Tim Weinitz <weinitz.tim@gmail.com>
2021-10-12 11:06:11 +0200

    Add comments to People

--- LexiconTodoIt/Data/People.cs
+++ LexiconTodoIt/Data/People.cs
@@ -4,26 +4,54 @@ using TodoIt.Model;
 
 namespace LexiconTodoIt.Data
 {
+
+    /// <summary>
+    /// Repository of persons
+    /// </summary>
     public class People
     {
+
+        /// <summary>
+        /// Array of persons
+        /// </summary>
         private static Person[] persons = new Person[0];
 
+        /// <summary>
+        /// Number of items in persons
+        /// </summary>
+        /// <returns>The total size of persons</returns>
         public int Size()
         {
             return persons.Length;
         }
 
+        /// <summary>
+        /// Find all elements in persons
+        /// </summary>
+        /// <returns>all persons</returns>
         public Person[] FindAll()
         {
             return persons;
         }
 
+        /// <summary>
+        /// Searches for an person that matches the ><paramref name="personId" />, and returns the first occurrence in persons" />.
+        /// </summary>
+        /// <param name="personId">The personId to be found</param>
+        /// <returns>The found person</returns>
         public Person FindById(int personId)
         {
+            
             var person = persons.First(e => e.PersonId == personId);
             return person;
         }
 
+        /// <summary>
+        /// Adds a person to persons.
+        /// </summary>
+        /// <param name="firstName">The first name of the person</param>
+        /// <param name="lastName">The last name of the person</param>
+        /// <returns>The added person</returns>
         public Person AddPerson(string firstName, string lastName)
         {
             var personId = PersonSequencer.nextPersonId();
@@ -36,17 +64,28 @@ namespace LexiconTodoIt.Data
             return person;
         }
 
+        /// <summary>
+        /// Clear persons
+        /// </summary>
         public void Clear()
         {
             persons = Array.Empty<Person>();
         }
 
+        /// <summary>Removes the a person based on the index.</summary>
+        /// <param name="person">The person to remove.</param>
         public void RemovePerson(Person person)
         {
             var personIndex = IndexOfPerson(person);
             persons = persons.Where((x, index) => !index.Equals(personIndex)).ToArray();
         }
 
+        /// <summary>
+        /// Searches for person, and returns the zero-based index of the first occurrence in persons .
+        /// </summary>
+        /// <param name="person">The person to find PersonId for</param>
+        /// <returns>The zero-based index of the first occurrence of a person that matches the PersonId, if found.</returns>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="person" /> is not found</exception>
         public int IndexOfPerson(Person person)
         {
             var personIndex = Array.FindIndex(persons, x => x.PersonId.Equals(person.PersonId));
